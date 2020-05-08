be5babf19986226796ec88f0244f99a8331a6296
Everything4info <54599875+Everything4info@users.noreply.github.com>
2020-05-09 01:50:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const apiRoutes = require(https://www.jsonstore.io/ba6cecff7d0039b393e36606e37692a2bea83de11f6212311041498998b1909e');
+const apiRoutes = require(https://www.jsonstore.io/1a77c7266d87ef3a04ba31a22e88fd303d62fac9c46c5a22791f7684a5f6644c');
 
 const port = process.env.PORT || 3000;
 const app = express();
